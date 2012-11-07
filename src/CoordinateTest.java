150548ce72c7bb51c6a57735045de861dd39cbdb
Jesse <jnels124@msudenver.edu>
2012-11-06 22:37:31 -0800

    Added test cases to verify correct Exception is thrown with bad arguments

--- src/CoordinateTest.java
+++ src/CoordinateTest.java
@@ -21,5 +21,34 @@ public class CoordinateTest extends TestCase {
         catch ( Exception e ) {
           fail( "Should not throw an exception." );
         }
+        
+        // Verify proper exception thrown on invalid column value
+        try {
+            new Coordinate(-1, 0);
+            fail("Exception should have been thrown");
+        }
+        catch ( UnsupportedOperationException e ) {
+            assertTrue(true);
+        } 
+        catch ( Exception e ) {
+            fail("Incorrect Exception thrown ");
+        }
+        
+        // Verify proper exception thrown on invalid row value
+        try {
+            new Coordinate(0, -1);
+            fail("Exception should have been thrown");
+        }
+        catch ( UnsupportedOperationException e ) {
+            assertTrue(true);
+        } 
+        catch ( Exception e ) {
+            fail("Incorrect Exception thrown ");
+        }
+	}
+	
+	public void getColumnTest() {
+	    
+	    
 	}
 }

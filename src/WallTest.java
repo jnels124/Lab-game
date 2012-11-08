b6b1c119d706a627ec5710c803d45335ed18def9
Josh Gillham <usajoshgillham@gmail.com>
2012-11-08 12:31:15 -0700

    Wrote GB test for Wall. Closes #3.

---
+++ src/WallTest.java
@@ -0,0 +1,51 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * The test class WallTest.
+ *
+ * @author  Josh Gillham
+ * @version (a version number or a date)
+ */
+public class WallTest {
+    /**
+     * Proves the constructor can create a Wall object without an error.
+     */
+    @Test
+    public void testConstructor() {
+        try {
+            Set< Direction > directions = new TreeSet< Direction >();
+            directions.add( Direction.North );
+            new Wall( directions );
+            directions.add( Direction.West );
+            new Wall( directions );
+            directions.add( Direction.South );
+            new Wall( directions );
+        } catch( Exception e ) {
+            fail( "Should not throw an exception" );
+        }
+    }
+    
+    /**
+     * Proves getDirections returns the same object as passed in the constructor.
+     */
+    @Test
+    public void testGetDirections() {
+        Set< Direction > directions = new TreeSet< Direction >();
+        directions.add( Direction.North );
+        Wall wall = new Wall( directions );
+        assertEquals( directions, wall.getDirections() );
+        directions.add( Direction.West );
+        wall = new Wall( directions );
+        assertEquals( directions, wall.getDirections() );
+        directions.add( Direction.South );
+        wall = new Wall( directions );
+        assertEquals( directions, wall.getDirections() );
+    }
+}

d45db80e80a6da0bcc5e327c900df32a60b85756
Josh Gillham <usajoshgillham@gmail.com>
2012-11-08 22:12:28 -0700

    Finished writing GB tests for Coordinate. Closes #6.

--- src/CoordinateTest.java
+++ src/CoordinateTest.java
@@ -54,6 +54,88 @@ public class CoordinateTest extends TestCase {
     }
     
     /**
+     * Proves the constructor can set the row.
+     */
+    @Test
+    public void testGetRow() {
+         Coordinate instance = new Coordinate(2,0);
+         assertEquals( 0, instance.getRow() );
+    }
+    
+    /**
+     * Proves the constructor can set the column.
+     */
+    @Test
+    public void testGetColumn() {
+         Coordinate instance = new Coordinate(2,0);
+         assertEquals( 2, instance.getColumn() );
+    }
+    
+    /**
+     * Proves the row getter can return the value of the setter.
+     */
+    @Test
+    public void testGetAndSetRow() {
+        Coordinate instance = new Coordinate(2,0);
+        instance.setRow( 5 );
+        assertEquals( 5, instance.getRow() );
+        instance.setRow( 15 );
+        assertEquals( 15, instance.getRow() );
+    }
+    
+    /**
+     * Proves the row getter can return the value of the setter.
+     */
+    @Test
+    public void testGetAndSetColumn() {
+        Coordinate instance = new Coordinate(2,0);
+        instance.setColumn( 5 );
+        assertEquals( 5, instance.getColumn() );
+        instance.setColumn( 15 );
+        assertEquals( 15, instance.getColumn() );
+    }
+    
+    /**
+     * Proves the translate will move the position of the coordinate in
+     *  the corresponding direction.
+     */
+    @Test
+    public void testTranslate() {
+        Coordinate instance = new Coordinate(3,3);
+        instance.translate( Direction.North );
+        assertEquals( 3, instance.getColumn() );
+        assertEquals( 2, instance.getRow() );
+        
+        instance.translate( Direction.NorthEast );
+        assertEquals( 4, instance.getColumn() );
+        assertEquals( 1, instance.getRow() );
+        
+        instance.translate( Direction.East );
+        assertEquals( 5, instance.getColumn() );
+        assertEquals( 1, instance.getRow() );
+        
+        instance.translate( Direction.SouthEast );
+        assertEquals( 6, instance.getColumn() );
+        assertEquals( 2, instance.getRow() );
+        
+        instance.translate( Direction.South );
+        assertEquals( 6, instance.getColumn() );
+        assertEquals( 3, instance.getRow() );
+        
+        instance.translate( Direction.SouthWest );
+        assertEquals( 5, instance.getColumn() );
+        assertEquals( 4, instance.getRow() );
+        
+        instance.translate( Direction.West );
+        assertEquals( 4, instance.getColumn() );
+        assertEquals( 4, instance.getRow() );
+        
+        instance.translate( Direction.NorthWest );
+        assertEquals( 3, instance.getColumn() );
+        assertEquals( 3, instance.getRow() );
+    }
+    
+    /**
      * Prove the object can be cloned.
      */
     @Test

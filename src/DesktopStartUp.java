b41318c9d1f6ac01278a705345bb74e7d4cc4317
Josh Gillham <usajoshgillham@gmail.com>
2012-11-13 11:56:58 -0700

    Drafted DesktopStartUp.java.

--- src/DesktopStartUp.java
+++ src/DesktopStartUp.java
@@ -6,12 +6,17 @@
  * @version 10-29-12
  */
 public class DesktopStartUp {
+    
     /**
      * Initializes the game and launches the UI.
      * 
      * @param args not used.
      */
     static public void main( String[] args ) {
-        throw new UnsupportedOperationException();
+        Logic game = new TestInstrumentLogic();
+        GUI ui = new GUI( game );
+        
     }
+    
+    
 }

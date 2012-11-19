2cdcfdb452bc3340515f7d3483e3f9f111c2d264
Josh Gillham <usajoshgillham@gmail.com>
2012-11-18 17:46:22 -0700

    Implemented DesktopStartUp. Closes #17.

--- src/DesktopStartUp.java
+++ src/DesktopStartUp.java
@@ -13,9 +13,13 @@ public class DesktopStartUp {
      * @param args not used.
      */
     static public void main( String[] args ) {
-        Logic game = new TestInstrumentLogic();
+        Wall[][] lab = {
+            { Wall.CORNERNW, Wall.SIDEN, Wall.CORNERNE  },
+            { Wall.SIDEW, Wall.EMPTY, Wall.SIDEE  },
+            { Wall.CORNERSW, Wall.SIDES, Wall.CORNERSW  }
+        };
+        Logic game = new Logic( new Character( new Coordinate( 1, 1 ) ), new Maze( lab ) );
         GUI ui = new GUI( game );
-        
     }
     
     
